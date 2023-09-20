57ed1a8198e8da26378e8675fa8fde2b4fd32eaf
Moldashbek Sultankul <sultankulmaksatuly@gmail.com>
2023-09-20 17:51:58 +0600

    dfdgbnh

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000 && salary >= 0) {
+            double taxedSalary = salary * 0.85; // 15% tax
+            System.out.println("Net Salary after taxes: " + taxedSalary);
+        } else if (salary > 10000 && salary <= 20000) {
+            double taxedSalary = salary * 0.82; // 18% tax
+            System.out.println("Net Salary after taxes: " + taxedSalary);
+        } else if (salary > 20000) {
+            double taxedSalary = salary * 0.8; // 20% tax
+            System.out.println("Net Salary after taxes: " + taxedSalary);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
